bf9ee2ee6ed586900817158191d63b479f7b5884
Sahil <sahilmankar311@gmail.com>
2023-05-02 14:05:36 +0530

    insert updated

--- EAgroServicesViews/Controllers/PurchaseController.cs
+++ EAgroServicesViews/Controllers/PurchaseController.cs
@@ -1,15 +1,15 @@
+using System.Text;
 using System.Threading.Tasks;
 using EAgroServicesViews.Models.PurchaseModels;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace EAgroServicesViews.Controllers;
-
+[Route("purchase")]
 public class PurchaseController : Controller
 {
-    // private static readonly HttpClient client = new HttpClient();
-
-    public async Task<IActionResult> GetAll()
+    [HttpGet("showall")]
+    public async Task<IActionResult> ShowAll()
     {
         List<PurchaseViewModel> items = new List<PurchaseViewModel>();
 
@@ -24,8 +24,53 @@ public class PurchaseController : Controller
         }
     }
 
-    public IActionResult Privacy()
+    [HttpGet("showbyid/{id}")]
+    public async Task<IActionResult> ShowById(int id)
+    {
+        PurchaseViewModel item = new PurchaseViewModel();
+
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.GetAsync("http://localhost:5171/api/purchase/getbyid/" + id))
+            {
+                var apiResponse = await response.Content.ReadAsStringAsync();
+                item = JsonConvert.DeserializeObject<PurchaseViewModel>(apiResponse);
+                return View(item);
+            }
+        }
+    }
+
+
+    [HttpGet("additem")]
+    public IActionResult AddItem()
     {
         return View();
+    }
+
+
+
+    [HttpPost("additem")]
+    public async Task<IActionResult> AddItem(PurchaseItem item)
+    {
+        var data = JsonConvert.SerializeObject(item);
+        var requestContent = new StringContent(data, Encoding.UTF8, "application/json");
+        using (var httpClient = new HttpClient())
+        {
+            using (var response = await httpClient.PostAsync("http://localhost:5171/api/purchase/insert", requestContent))
+            {
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                string res = JsonConvert.DeserializeObject<string>(content);
+                if (res == "true")
+                {
+                    return RedirectToAction("ShowAll");
+                }
+                else
+                {
+                    Console.WriteLine("Error while adding");
+                    return RedirectToAction("Error");
+                }
+            }
+        }
     }
 }
