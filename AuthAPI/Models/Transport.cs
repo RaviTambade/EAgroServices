db8f55e41c736e46adec36819d2b27ef5eba8897
Sahil <sahilmankar311@gmail.com>
2023-05-10 15:45:39 +0530

    ui updated

---
+++ AuthAPI/Models/Transport.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AuthAPI.Models;
+public class Transport
+{
+    [Column("transport_id")]
+    public int TransportId { get; set; }
+
+    [Column("office_name")]
+    public string? OfficeName { get; set; }
+
+    [Column("first_name")]
+    public string? FirstName { get; set; }
+
+    [Column("last_name")]
+    public string? LastName { get; set; }
+
+    [Column("location")]
+    public string? Location { get; set; }
+
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+}
