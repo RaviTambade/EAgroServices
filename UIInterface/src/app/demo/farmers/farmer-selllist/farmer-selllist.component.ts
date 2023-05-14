a61b79ad1ceed3e114d9079e86be0d1eda7a617c
Sahil <sahilmankar311@gmail.com>
2023-05-14 09:57:13 +0530

    filters added

--- UIInterface/src/app/demo/farmers/farmer-selllist/farmer-selllist.component.ts
+++ UIInterface/src/app/demo/farmers/farmer-selllist/farmer-selllist.component.ts
@@ -31,8 +31,43 @@ export class FarmerSelllistComponent {
           this.purchaseViewModel = response;
           console.log(this.purchaseViewModel);
         })
-
       }
+
+      onSortByVarietyNameChange(event:Event) {
+        const target = event.target as HTMLInputElement;
+        const order = target.value;
+        this.purchaseViewModel = this.sortPurchasesByVarietyName(this.purchaseViewModel, order);
+      }
+      
+      onSortByAmountChange(event:Event) {
+        const target = event.target as HTMLInputElement;
+        const order = target.value;
+        this.purchaseViewModel = this.sortPurchasesByAmount(this.purchaseViewModel, order);
+      }
+      
+      sortPurchasesByVarietyName(purchases: Purchaseviewmodel[], sortOrder: string): Purchaseviewmodel[] {
+        let sortedPurchases = purchases.slice();
+        if (sortOrder === 'asc') {
+          sortedPurchases.sort((a, b) => a.varietyName.localeCompare(b.varietyName));
+        } else {
+          sortedPurchases.sort((a, b) => b.varietyName.localeCompare(a.varietyName));
+        }
+        return sortedPurchases;
+      }
+      
+      sortPurchasesByAmount(purchases: Purchaseviewmodel[], sortOrder: string): Purchaseviewmodel[] {
+        let sortedPurchases = purchases.slice();
+        if (sortOrder === 'asc') {
+          sortedPurchases.sort((a, b) => a.purchaseItem.amount - b.purchaseItem.amount);
+        } else {
+          sortedPurchases.sort((a, b) => b.purchaseItem.amount - a.purchaseItem.amount);
+        }
+        return sortedPurchases;
+      }
+      
+      
+      
+
     }
   
 
--- UIInterface/src/app/demo/farmers/purchaseviewmodel.ts
+++ UIInterface/src/app/demo/farmers/purchaseviewmodel.ts
@@ -2,27 +2,10 @@ import { PurchaseItem } from "./purchase-item";
 import { Purchasebilling } from "./purchasebilling";
 
 export class Purchaseviewmodel {
-    farmerName: string;
-    purchaseBilling: {
-        billId: number,
-        date: string,
-        labourCharges: number,
-        purchaseId: number,
-        totalAmount: number
-    };
-    purchaseItem: {
-        amount: number,
-        containerType: string,
-        date: string,
-        farmerId: number,
-        grade: string,
-        netWeight: number,
-        purchaseId: number,
-        quantity: number,
-        ratePerKg: number,
-        tareWeight: number,
-        totalWeight: number,
-        varietyId: number
-    };
-    varietyName: string;
+  constructor(
+    public farmerName: string,
+    public purchaseBilling: Purchasebilling,
+    public purchaseItem: PurchaseItem,
+    public varietyName: string
+  ){}
 }
