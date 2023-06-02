2bda60287f8376a8703100d772fc7e32031bcfd0
Sahil <sahilmankar311@gmail.com>
2023-06-02 21:33:10 +0530

    unwanted code delated

--- PurchasingAPI/Models/FarmerOrder.cs
+++
@@ -1,7 +0,0 @@
-namespace PurchasingAPI.Models;
-public class FarmerOrder
-{
-    public int OrderCount { get; set; }
-    public string? Month { get; set; }
-    public int Year { get; set; }
-}
--- PurchasingAPI/Models/FarmerSellMonth.cs
+++
@@ -1,7 +0,0 @@
-namespace PurchasingAPI.Models;
-public class FarmerSellMonth
-{
-    public double TotalAmount{get;set;}
-    public  string? Month{get;set;}
-    public int Year {get;set;}
-}
