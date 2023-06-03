31595755008f02339e2ee43ff0d77c08d76ae05b
Shubham Teli <shubhamteli203@gmail.com>
2023-06-03 14:19:25 +0530

    Update Employee.cs

--- EmployeesAPI/Models/Employee.cs
+++ EmployeesAPI/Models/Employee.cs
@@ -1,16 +1,1 @@
-// using System.ComponentModel.DataAnnotations.Schema;
-// namespace EmployeesAPI.Models;
-// public class Employee
-// {
-//     [Column("id")]
-//     public int Id { get; set; }
-//     [Column("firstname")]
-//     public string? FirstName { get; set; }
-//     [Column("lastname")]
-//     public string? LastName { get; set; }
-//     [Column("location")]
-//     public string? Location { get; set; }
-//     [Column("userid")]
-//     public int UserId { get; set; }
-// }
 
