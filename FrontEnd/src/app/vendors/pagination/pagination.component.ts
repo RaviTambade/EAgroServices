4c9d002bd3fdc6f83010579a141907312854007e
Sahil <sahilmankar311@gmail.com>
2023-06-13 09:02:43 +0530

    check added for page

--- FrontEnd/src/app/vendors/pagination/pagination.component.ts
+++ FrontEnd/src/app/vendors/pagination/pagination.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
-  items = ["abh", "ay", "yui", "ioi", "yui", "uio", "uio", "opo", "hjh", "yuy", "etrty", "uio", "opo", "hjh", "yuy", "etrty"];
+  items = ["abh", "ay", "yui", "ioi", "yui", "uio", "uio", "opo", "hjh", "yuy", "etrty", "uio", "opo", "hjh", "yuy","abc"];
   currentPage = 0;
   arrLenght = 0;
   constructor() {
@@ -22,9 +22,14 @@ export class PaginationComponent {
     this.currentPage++;
   }
   hasNextPage(): boolean {
-    const totalpages = Math.trunc(this.arrLenght / 5)
+    const totalpages = Math.trunc(this.arrLenght / 5);
     console.log("🚀 ~ hasnextPage ~ totalpages:", totalpages);
-    if (this.currentPage < totalpages) {
+   
+    if(this.arrLenght %5 ==0){
+      return this.currentPage < totalpages-1;
+    }
+
+    if (this.currentPage < totalpages ) {
       return true;
     }
     return false;
