c3122189a2f81de48b93d6e8b28bce696550a776
unknown <shubhamteli203@gmail.com>
2023-06-13 11:05:04 +0530

    Details

--- FrontEnd/src/app/vendors/collectionviewmodel.ts
+++ FrontEnd/src/app/vendors/collectionviewmodel.ts
@@ -2,7 +2,7 @@
 export class Collectionviewmodel {
     constructor(public id: number,
                public farmerName: string, 
-               public cropName: string,
+               public crop: string,
                public containerType:string,
                public quantity:number,
                public date:string) { }
