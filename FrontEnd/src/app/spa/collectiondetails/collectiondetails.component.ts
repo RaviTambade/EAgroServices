3ee1367f2b5daa88302a826a3e3a9082d5a038ed
unknown <shubhamteli203@gmail.com>
2023-06-13 13:22:59 +0530

    Edit

--- FrontEnd/src/app/spa/collectiondetails/collectiondetails.component.ts
+++ FrontEnd/src/app/spa/collectiondetails/collectiondetails.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Collection } from 'src/app/vendors/collection';
 import { Collectionviewmodel } from 'src/app/vendors/collectionviewmodel';
 import { CollectionService } from '../collection.service';
@@ -14,17 +14,20 @@ export class CollectiondetailsComponent implements OnInit{
   @Output() sendCollection =new EventEmitter();
  @Input() collectionId:number |any;
 collection:Collection |any;
-  constructor(private route: ActivatedRoute,public svc:CollectionService){}
+  constructor(private route: ActivatedRoute,public svc:CollectionService,private router:Router){}
   ngOnInit(): void {
     this.collectionId=this.route.snapshot.paramMap.get('id');
     this.svc.getCollection(this.collectionId).subscribe((response)=>{
       this.collection=response;
-      this.sendCollection.emit({ollection:this.collection})
+      this.sendCollection.emit({collection:this.collection})
       console.log(response);
     }
+    
 
     )
   }
+ 
+  }
 
 
-}
+
