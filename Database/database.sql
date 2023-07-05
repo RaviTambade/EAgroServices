1854496dc376b8a83dba3283662edf08efc09a60
Sahil <sahilmankar311@gmail.com>
2023-07-05 22:39:00 +0530

    procedure added

--- Database/database.sql
+++ Database/database.sql
@@ -1,4 +1,4 @@
--- Active: 1676969830187@@127.0.0.1@3306@eagroservicesdb
+-- Active: 1682349138553@@127.0.0.1@3306@eagroservicesdb
 Drop DATABASE IF EXISTS eagroservicesdb;
 CREATE DATABASE eagroservicesdb;
 USE eagroservicesdb;
@@ -108,7 +108,7 @@ CREATE TABLE
         id INT PRIMARY KEY AUTO_INCREMENT,
         vehicleid INT,
         merchantid INT,
-        kilometer INT,
+        kilometers INT,
         shipmentdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         CONSTRAINT fk_shipment_vehicle FOREIGN KEY (vehicleid) REFERENCES vehicles(id) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT fk_shipment_users FOREIGN KEY (merchantid) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -126,17 +126,101 @@ CREATE TABLE
         id INT  AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(50),
         description VARCHAR(50),
-         amount  DOUBLE
+        amount  DOUBLE
     );
 CREATE TABLE goodscosting(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    shippingitemsid INT NOT NULL,
-    frieghtcharges DOUBLE,
+    shippingitemsid INT NOT NULL UNIQUE,
+    freightcharges DOUBLE,
     labourcharges DOUBLE,
-    ownercharges DOUBLE,
+    serviceharges DOUBLE,
     CONSTRAINT fk_goodscosting_shippingitems FOREIGN KEY (shippingitemsid) REFERENCES shippingitems(id) ON UPDATE CASCADE ON DELETE CASCADE
 
 );
+CREATE PROCEDURE apply_labour_charges(IN shipping_items_id INT)
+BEGIN
+DECLARE labourcharges DOUBLE DEFAULT 0;
+-- DECLARE collection_id INT DEFAULT 0;
+-- DECLARE bags INT DEFAULT 0;
+-- DECLARE labourrateperbag INT DEFAULT 0;
+-- DECLARE container_type varchar(20);
+-- SELECT collectionid INTO collection_id FROM shippingitems WHERE id=shipping_items_id;
+-- SELECT containertype,quantity INTO container_type,bags FROM goodscollections WHERE id=collection_id;
+-- SELECT amount INTO labourrateperbag FROM ratecard WHERE title=container_type;
+-- SET labourcharges= bags * labourrateperbag ;
+
+SELECT (goodscollections.quantity*ratecard.amount) INTO labourcharges FROM goodscollections
+JOIN shippingitems ON goodscollections.id = shippingitems.collectionid
+JOIN ratecard ON goodscollections.containertype = ratecard.title
+WHERE shippingitems.id = shipping_items_id;
+
+INSERT INTO goodscosting(shippingitemsid,labourcharges) VALUES (shipping_items_id,labourcharges);
+END;
+
+CREATE PROCEDURE apply_service_charges(IN shipping_items_id INT)
+BEGIN
+    UPDATE goodscosting
+    JOIN shippingitems ON goodscosting.shippingitemsid = shippingitems.id
+    JOIN verifiedgoodscollection ON shippingitems.collectionid = verifiedgoodscollection.collectionid
+    JOIN ratecard ON ratecard.title = "servicecharges"
+    SET goodscosting.servicecharges = verifiedgoodscollection.weight * ratecard.amount
+    WHERE shippingitems.id = shipping_items_id;
+END;
+
+CREATE PROCEDURE apply_total_freight_charges(IN shipment_id INT, OUT total_freight_charges DOUBLE)
+BEGIN
+    SELECT (shipments.kilometer * ratecard.amount) INTO total_freight_charges
+    FROM shipments
+    INNER JOIN vehicles ON shipments.vehicleid = vehicles.id
+    INNER JOIN ratecard ON vehicles.vehicletype = ratecard.title
+    WHERE shipments.id = shipment_id;
+END;
+
+CREATE PROCEDURE apply_freight_charges_for_collection(IN shipment_id INT) 
+BEGIN 
+	DECLARE finished INT DEFAULT 0;
+	DECLARE shipping_item_id INT DEFAULT 0;
+	DECLARE freight_charges DOUBLE DEFAULT 0;
+	DECLARE totalfreightcharges DOUBLE DEFAULT 0;
+	DECLARE collection_weight DOUBLE DEFAULT 0;
+	DECLARE total_collections_weight DOUBLE DEFAULT 0;
+	DECLARE freight_rate_per_kg DOUBLE DEFAULT 0;
+
+	DECLARE cursor_shipment_item_id CURSOR FOR
+	SELECT id FROM shippingitems WHERE shipmentid = shipment_id;
+
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+
+	CALL apply_total_freight_charges(shipment_id,@total_freight_charges);
+	SELECT @total_freight_charges INTO totalfreightcharges;
+
+	SELECT SUM(weight) INTO total_collections_weight FROM verifiedgoodscollection
+	INNER JOIN shippingitems ON verifiedgoodscollection.collectionid = shippingitems.collectionid
+	WHERE shippingitems.shipmentid = shipment_id;
+
+	SET freight_rate_per_kg=totalfreightcharges/total_collections_weight;
+
+	OPEN cursor_shipment_item_id;
+	updatefreightcharges: LOOP
+	    FETCH cursor_shipment_item_id INTO shipping_item_id;
+
+        IF finished = 1 THEN 
+            LEAVE updatefreightcharges;
+        END IF;
+
+        SELECT weight INTO collection_weight FROM verifiedgoodscollection
+        INNER JOIN shippingitems ON verifiedgoodscollection.collectionid = shippingitems.collectionid
+        WHERE shippingitems.id = shipping_item_id;
+
+        SET freight_charges = freight_rate_per_kg * collection_weight;
+
+        UPDATE goodscosting SET freightcharges = freight_charges WHERE shippingitemsid = shipping_item_id;
+	END LOOP updatefreightcharges;
+	CLOSE cursor_shipment_item_id;
+END; 
+
+
+
 
 CREATE TABLE
     invoices (
