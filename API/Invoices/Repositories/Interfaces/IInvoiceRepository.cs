f863013410b1161f8cddc1c9eec376b0418ebe67
unknown <shubhamteli203@gmail.com>
2023-07-29 22:49:16 +0530

    collection Invoice

--- API/Invoices/Repositories/Interfaces/IInvoiceRepository.cs
+++ API/Invoices/Repositories/Interfaces/IInvoiceRepository.cs
@@ -5,6 +5,7 @@ namespace Invoices.Repositories.Interfaces
     public interface IInvoiceRepository
     {
         Task<List<InvoiceDetails>> GetAll(int merchantId,string paymentStatus);
+        Task<InvoiceChargesDetails> GetInvoice(int collectionId);
         Task<InvoiceChargesDetails> GetById(int invoiceId);
 
         Task<bool> Insert(Invoice invoice);
