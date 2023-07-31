122cb44e36f21b44fbd644d9e1a2211579ec4e70
Shubham Teli <shubhamteli203@gmail.com>
2023-07-31 21:56:12 +0530

    Update Vehicle.cs

--- API/Vehicles/Models/Vehicle.cs
+++ API/Vehicles/Models/Vehicle.cs
@@ -1,7 +1,7 @@
 namespace Vehicles.Models;
 public class Vehicle{
-    public int Id{get;set;}
-    public int TransporterId{get;set;}
-    public string VehicleType{get;set;}
-    public string RtoNumber{get;set;}
+    public int Id { get; set; }
+    public int TransporterId { get; set; }
+    public string VehicleType { get; set; }
+    public string RtoNumber { get; set; }
 }
