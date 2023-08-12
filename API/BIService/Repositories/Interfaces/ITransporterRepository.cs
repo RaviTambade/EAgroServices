4c6dbfad34bd99013d42781a35d57603280c2d21
NavaleAbhay <navale.abhay12@gmail.com>
2023-08-12 23:03:04 +0530

    code restructured

--- API/BIService/Repositories/Interfaces/ITransporterRepository.cs
+++ API/BIService/Repositories/Interfaces/ITransporterRepository.cs
@@ -3,4 +3,5 @@ namespace BIService.Repositories.Interfaces;
 public interface ITransporterRepository
 {
     Task<List<YearlyVehicleRevenue>> GetRevenuesByYear(int transporterId,int year);
+    Task<List<int>> GetYears();
 }
