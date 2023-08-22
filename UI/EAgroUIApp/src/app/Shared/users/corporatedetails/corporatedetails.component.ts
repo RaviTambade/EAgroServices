9e60ad67fc27e49ded74960adaaf66963d112809
NavaleAbhay <navale.abhay12@gmail.com>
2023-08-22 12:48:08 +0530

    corporate details updated for merchant and transporter

--- UI/EAgroUIApp/src/app/Shared/users/corporatedetails/corporatedetails.component.ts
+++ UI/EAgroUIApp/src/app/Shared/users/corporatedetails/corporatedetails.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CollectioncenterService } from 'src/app/collectioncenter.service';
 import { CorporateService } from 'src/app/corporate.service';
 import { Corporation } from 'src/app/membership/corporation';
+import { MerchantService } from 'src/app/merchant/merchant.service';
+import { TransporterService } from 'src/app/transporter/transporter.service';
 
 @Component({
   selector: 'app-corporatedetails',
@@ -9,24 +11,47 @@ import { Corporation } from 'src/app/membership/corporation';
   styleUrls: ['./corporatedetails.component.css']
 })
 export class CorporatedetailsComponent implements OnInit {
-corporateId:any
-corporation:Corporation ={
-  id: 0,
-  name: '',
-  contactNumber: '',
-  email: '',
-  personId: 0
-}
-constructor(private collectionCenterSvc:CollectioncenterService,private crpSvc:CorporateService
-){}
+  corporateId: any
+  corporation: Corporation = {
+    id: 0,
+    name: '',
+    contactNumber: '',
+    email: '',
+    personId: 0
+  }
+  constructor(private transportSvc: TransporterService,
+    private collectionCenterSvc: CollectioncenterService,
+    private crpSvc: CorporateService,
+    private merchantSvc: MerchantService
+  ) { }
   ngOnInit(): void {
-   this.collectionCenterSvc.getCorporateIdByCollectionCenterId().subscribe((res)=>{
-    this.corporateId=res
-    this.crpSvc.getCorporateDetails(this.corporateId).subscribe((res)=>{
-      this.corporation=res
-      console.log(res)
-    })
-
-   })
+    let role = localStorage.getItem("role")
+    if (role == 'collection manager') {
+      this.collectionCenterSvc.getCorporateIdByCollectionCenterId().subscribe((res) => {
+        this.corporateId = res
+        this.crpSvc.getCorporateDetails(this.corporateId).subscribe((res) => {
+          this.corporation = res
+          console.log(res)
+        })
+      })
+    }
+    if (role == 'transporter') {
+      this.transportSvc.getCorporateIdOfTransporter().subscribe((res) => {
+        this.corporateId = res
+        this.crpSvc.getCorporateDetails(this.corporateId).subscribe((res) => {
+          this.corporation = res
+          console.log(res)
+        })
+      })
+    }
+    if (role == 'merchant') {
+      this.merchantSvc.getMerchantCorporateId().subscribe((res) => {
+        this.corporateId = res
+        this.crpSvc.getCorporateDetails(this.corporateId).subscribe((res) => {
+          this.corporation = res
+          console.log(res)
+        })
+      })
+    }
   }
 }
