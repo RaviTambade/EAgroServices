e1d08719c707be6d4e3a3c96dc40d69246deb961
Shubham Teli <shubhamteli203@gmail.com>
2023-08-28 22:27:21 +0530

    Delete UI/EAgroServicesViews directory

--- UI/EAgroServicesViews/Models/PurchaseModels/PurchaseViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EAgroServicesViews.Models.PurchaseModels;
-public class PurchaseViewModel
-{
-    public PurchaseItem? PurchaseItem{get;set;}
-    public PurchaseBilling? PurchaseBilling{get;set;}
-    public string? FarmerName{get;set;}
-    public string? VarietyName{get;set;}
-}
