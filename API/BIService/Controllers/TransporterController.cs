19be4ca9332817f2c4fa9b39f7c0e3d0b4ab4a7d
Shubham Teli <shubhamteli203@gmail.com>
2023-09-04 22:17:32 +0530

    Update TransporterController.cs

--- API/BIService/Controllers/TransporterController.cs
+++ API/BIService/Controllers/TransporterController.cs
@@ -6,45 +6,45 @@ namespace Transflower.EAgroServices.BIService.Controllers;
 [Route("/api/transporterbi")]
 public class TransporterBIController : ControllerBase
 {
-  private readonly ITransporterService _srv;
-  public TransporterBIController(ITransporterService srv)
+  private readonly ITransporterService _service;
+  public TransporterBIController(ITransporterService service)
   {
-    _srv = srv;
+    _service = service;
   }
 
   [HttpGet("revenue/year/{transporterId:int}/{year:int}")]
   public async Task<List<YearlyVehicleRevenue>> GetRevenuesByYear(int transporterId, int year)
   {
-    return await _srv.GetRevenuesByYear(transporterId, year);
+    return await _service.GetRevenuesByYear(transporterId, year);
   }
 
   [HttpGet("years/{transporterId}")]
   public async Task<List<int>> GetYears(int transporterId)
   {
-    return await _srv.GetYears(transporterId);
+    return await _service.GetYears(transporterId);
   }
 
   [HttpGet("revenue/month/{transporterId:int}/{year:int}")]
   public async Task<List<MonthRevenue>> GetMonthlyRevenue(int transporterId, int year)
   {
-    return await _srv.GetMonthlyRevenue(transporterId, year);
+    return await _service.GetMonthlyRevenue(transporterId, year);
   }
 
   [HttpGet("revenue/quarter/{transporterId:int}/{year:int}")]
   public async Task<List<QuarterRevenue>> GetRevenuesByQuarter(int transporterId, int year)
   {
-    return await _srv.GetRevenuesByQuarter(transporterId, year);
+    return await _service.GetRevenuesByQuarter(transporterId, year);
   }
 
   [HttpGet("revenue/week/{transporterId:int}/{year:int}")]
   public async Task<List<WeekRevenue>> GetRevenuesByWeek(int transporterId, int year)
   {
-    return await _srv.GetRevenuesByWeek(transporterId, year);
+    return await _service.GetRevenuesByWeek(transporterId, year);
   }
 
   [HttpGet("revenue/year/{transporterId:int}")]
   public async Task<List<YearRevenue>> GetRevenueByYear(int transporterId)
   {
-    return await _srv.GetRevenueByYear(transporterId);
+    return await _service.GetRevenueByYear(transporterId);
   }
 }
