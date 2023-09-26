4372c995782b43c86cc9a492fa774384a18f8985
Shubham Teli <shubhamteli203@gmail.com>
2023-09-26 21:24:24 +0530

    Update collectioncenter.service.ts

--- UI/LatestNGApp/UIAgroStdApp/src/app/Services/collectioncenter.service.ts
+++ UI/LatestNGApp/UIAgroStdApp/src/app/Services/collectioncenter.service.ts
@@ -6,16 +6,6 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CollectioncenterService {
-  constructor(private http: HttpClient) {}
- 
-
-  // getCorporateIdByCollectionCenterId(): Observable<number> {
-  //   let collectionCenterId = localStorage.getItem('collectionCenterId');
-  //   let url =
-  //     'http://localhost:5192/api/collectioncenters/corporateid/' +
-  //     collectionCenterId;
-  //   return this.http.get<number>(url);
-  // }
 
 
 }
